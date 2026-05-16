3ba0c2757ca488ef64225afb60fd4d7d7b35b6dd
jgomezz <jgomezz@gmail.com>
2026-05-16 12:56:38 -0500

    update

--- sesion_01/s1_lab4_basic_agent_langchain_ollama.py
+++ sesion_01/s1_lab4_basic_agent_langchain_ollama.py
@@ -41,15 +41,34 @@ def calcular(expresion:str) -> str:
     except Exception as e:
         return f"Error al calcular: {str(e)}"
 
+@tool
+def informacion_Peru(consulta:str) -> str:
+    """
+    Contiene información sobre el Perú. Usalo para preguntas sobre PBI y  población.
+    """
+    # Aquí podrías implementar una consulta real a una base de datos o API
+    informacion = {
+        "pbi": "El PBI del Perú en 2025 fue de 340 mil millones de dólares.",
+        "población": "La población del Perú en 2025 fue de aproximadamente 34 millones de habitantes."
+    }
+    consulta = consulta.lower()
+    for clave, valor in informacion.items():
+        if clave in consulta or any(palabra in consulta for palabra in clave.split()):
+#        if clave in consulta:
+            return valor
+    return "No tengo información sobre esa consulta específica."
+
 
 basic_agent = create_agent(
     model = llm,
     tools = [ 
                 calcular ,
-                obtener_informacion_Tecsup
+                obtener_informacion_Tecsup,
+                informacion_Peru
             ],
     )
 
+'''
 result = basic_agent.invoke(
     {"messages": [("human", "¿Cuánto es 4827 * 3961?")]}
 )
@@ -59,9 +78,20 @@ result = basic_agent.invoke(
     {"messages": [("human", "Dame información de TECSUP")]}
 )
 print(f"Respuesta: {result['messages'][-1].content}")
-
-
 '''
+
+result = basic_agent.invoke(
+    {"messages": [("human", "Dame el valor del PBI del Perú")]}
+)
+print(f"Respuesta: {result['messages'][-1].content}")
+
+result = basic_agent.invoke(
+    {"messages": [("human", "Dame el valor de la poblacion del Perú")]}
+)
+print(f"Respuesta: {result['messages'][-1].content}")
+
+
+
 # Mostrar el historial de mensajes y llamadas a herramientas
 
 for msg in result["messages"]:
@@ -72,4 +102,5 @@ for msg in result["messages"]:
     if hasattr(msg, 'content') and msg.content:
         print(f"  {msg.content[:200]}")
 
-'''
+
+# Ejercicio : Crear un funcion que me obtenga la fecha y hora del dia, 
