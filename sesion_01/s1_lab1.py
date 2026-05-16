20459356acc9bfe82c9544627fac8cfc730232fc
jgomezz <jgomezz@gmail.com>
2026-05-16 10:04:43 -0500

    update

--- sesion_01/s1_lab1.py
+++ sesion_01/s1_lab1.py
@@ -4,8 +4,17 @@ import os
 load_dotenv()
 
 name = os.getenv("NAME","Unknown")
-
 path = os.getenv("PATH","Unknown")
-
 print("NAME:", name)
 print("PATH:", path)
+
+
+from openai import OpenAI
+
+model = os.getenv("LLAMA_MODEL","llama3.1:8b")
+base_url = os.getenv("LLAMA_BASE_URL","http://localhost:11434")
+
+
+llm = OpenAI(
+    base_url=base_url + "/v1"
+)
