0385cd9e741bba7b99e3371270b2823c65fb7eb2
jgomezz <jgomezz@gmail.com>
2026-05-23 10:43:52 -0500

    update

--- sesion_02/s2_lab2_1_advance_tools.py
+++ sesion_02/s2_lab2_1_advance_tools.py
@@ -1,4 +1,6 @@
 from langchain_core.tools import tool
+from langgraph.graph import END, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode
 import requests
 
 from model import get_llm
@@ -7,7 +9,7 @@ from model import get_llm
 def get_exchange_rate(query:str) -> str:
     """
         Busca información actualizada del tipo de cambio del dolar en el BCRP y
-        lo devuelve en soles por dolar
+        lo devuelve en soles peruanos. Utiliza la API del BCRP para obtener el tipo de cambio más reciente.
     """
     fecha_base = "2026-05-21"
     url = "https://estadisticas.bcrp.gob.pe/estadisticas/series/api/PD04640PD/json/" + fecha_base 
@@ -18,18 +20,80 @@ tools = [get_exchange_rate]
 
 llm = get_llm(name="ollama")
 
+llm_with_tools = llm.bind_tools(tools)
+
 
 # LangGraph
 
 
-if __name__ == "__main__":
+# ----------------------------------------
+# 1.- Definir los Nodos del grafo
+# ----------------------------------------
 
+# Reasoning Node
+def reasoning(state: MessagesState) -> dict:
+    """
+        Nodo de razonamiento que decide qué herramienta usar en base a la consulta del usuario
+    """
+    messages = state["messages"]
+
+    reponse = llm_with_tools.invoke(messages)
+
+    return {"messages": [reponse]}
+
+# Tool Node
+tool_executer = ToolNode(tools)
+
+# ----------------------------------------
+# 2.- Definir la logica de decisión del grafo
+# ----------------------------------------
+def decision_logic(state: MessagesState) -> str:
+    """
+        Lógica de decisión que determina qué herramienta usar en base a la consulta del usuario
+    """
+    last_message = state["messages"][-1]
+
+    if hasattr(last_message, 'tool_calls') and last_message.tool_calls:
+        return "tools"
+    else:
+        return "end"
+
+# ----------------------------------------
+# 3.- Construir el grafo
+# ----------------------------------------
+
+graph = StateGraph(MessagesState)
+
+# Agregar nodos
+graph.add_node("reasoning", reasoning)
+graph.add_node("tools", tool_executer)
+
+# Definir el inicio del grafo
+graph.set_entry_point("reasoning")
+
+# Agregar los Edges entre los nodos
+graph.add_conditional_edges(
+    "reasoning",  # Nodo de razonamiento
+    decision_logic,  # Lógica de decisión
+    {
+        "tools": "tools",  # Si la lógica decide usar herramientas, ir al nodo "tools"
+        "end" : END        # Si la lógica decide no usar herramientas, terminar el grafo
+    }
+)
+
+graph.add_edge("tools", "reasoning")  # Después de ejecutar la herramienta, volver al nodo de razonamiento
+
+# Compilar el grafo
+agent = graph.compile()
+
+
+if __name__ == "__main__":
     query = "¿Cuál es el tipo de cambio del dolar hoy?"
+    
+    '''
     response = get_exchange_rate(query)
     print(response)
+    '''
 
-'''
-{ "config": { "title":"Tipo de cambio", 
-              "series": [ { "name":"Tipo de cambio - TC Sistema bancario SBS (S/ por US$) - Venta", "dec":"3" }] }, 
-              "periods": [ { "name":"21.May.26", "values":["3.418"] } ] }
-'''
+    result =  agent.invoke({"messages": [("human", query)]})
+    print(f"Respuesta: {result['messages'][-1].content}")
