a5fbe68fdcafbd6c1b163bc215bc814ae91cd58c
jgomezz <jgomezz@gmail.com>
2026-06-06 11:39:45 -0500

    add fastapi application

---
+++ sesion_04/s4_lab1_1_basic_fastapi.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
+    
+app = FastAPI(title="Basic API")
+
+# ════════════════════════════════════════════════════════════
+# 1. API REST
+# ════════════════════════════════════════════════════════════
+
+@app.get("/chat")
+async def chat(): 
+    return {"mensaje": "Hola Mundo"}
+
+# ════════════════════════════════════════════════════════════
+# 3. EJECUTAR
+# ════════════════════════════════════════════════════════════
+
+if __name__ == "__main__":
+    
+    uvicorn.run(app, host="localhost", port=8000)
+
+
+
+
